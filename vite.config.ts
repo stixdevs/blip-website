1fe63ff32dd334996cb4c1e582e2fcd0f31e33c8
stixdevs <suorsaco@gmail.com>
2026-03-02 00:27:43 -0500

    fix: lovable vs github pages base url bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/blip-website/",
+  base: mode === "production" ? "/blip-website/" : "/",
 
   server: {
     host: "::",
